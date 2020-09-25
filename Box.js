b88b4bf3f4a5a98c87882e3c18a972b6ace09494
deepika201607 <65445244+deepika201607@users.noreply.github.com>
2020-09-25 15:24:43 +0530

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,18 @@
+class Box{
+constructor(){
+    var options={
+       'restitution':0.8
+        
+        }
+
+this.body = Bodies.rectangle(200,100,50,50,options);
+World.add(world,this.body);
+}
+display(){
+var pos = this.body.position;
+rectMode(CENTER);
+fill(255);
+  rect(pos.x,pos.y,this.width,this.height); 
+
+}
+}
